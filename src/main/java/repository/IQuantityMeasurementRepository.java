dd534f3c09f2dd04e162cffbd74a5dac91f475e4
Harshita Agrawal <harshiagrawal3459@gmail.com>
2026-03-17 17:41:37 +0530

    [Harshita Agrawal] add . UC16 - Database Integration

---
+++ src/main/java/repository/IQuantityMeasurementRepository.java
@@ -0,0 +1,22 @@
+package repository;
+import java.util.List;
+import entity.QuantityMeasurementEntity;
+
+public interface IQuantityMeasurementRepository {
+
+	    void save(QuantityMeasurementEntity entity);
+
+	    List<QuantityMeasurementEntity> getMeasurementsByOperation(String operation);
+	    
+	    List<QuantityMeasurementEntity> getMeasurementsByType(String type);
+	    
+	    int getTotalCount();
+	    
+	    void deleteAll();
+	    
+	    String getPoolStatistics();
+	    
+	    void releaseResources();
+
+		List<QuantityMeasurementEntity> findAll();
+}
