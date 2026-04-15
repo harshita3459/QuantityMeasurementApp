94fa4fa91b63f9072be4c240e47afd55fea3cfcb
Harshita Agrawal <harshiagrawal3459@gmail.com>
2026-04-15 21:51:35 +0530

    Revert "[Harshita Agrawal] add, application file for deployment"
    This reverts commit 0bf915039a3ef287f8c8f34c8694f310b5316137.

--- qma-service/src/main/java/com/app/quantityservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.quantityservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- qma-service/src/main/java/com/app/quantityservice/repository/QuantityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.quantityservice.repository;
-
-import com.app.quantityservice.entity.Quantity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuantityRepository extends JpaRepository<Quantity, Long> {
-}
